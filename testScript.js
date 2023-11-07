48d184c06f587baadf65aaf5e424972a2bddf2c7
Anderdragoon <anderdragonltl@outlook.com>
2023-11-06 22:51:26 -0500

    move with mouse

--- testScript.js
+++ testScript.js
@@ -1,59 +1,18 @@
-document.getElementById('game-container').addEventListener('mousedown', (e) => {
-    console.log('Mouse down event triggered');
+document.addEventListener('DOMContentLoaded', (event) => {
     const block = document.getElementById('block');
     let posX = 0;
     let posY = 0;
-    let targetX = 0; // 目标位置X
-    let targetY = 0; // 目标位置Y
-    let moving = false; // 是否正在移动
 
-    // 加速度和减速度
-    const acceleration = 0.5;
-    const deceleration = 0.5;
+    function followMouse(e) {
+        // 获取鼠标位置
+        posX = e.clientX - block.clientWidth / 2;
+        posY = e.clientY - block.clientHeight / 2;
 
-    // 动画循环函数
-    function animate() {
-        // 只有当方块的目标位置与当前位置不同时，才执行移动逻辑
-        if (moving) {
-            // 计算方块到目标位置的距离
-            const distX = targetX - posX;
-            const distY = targetY - posY;
-
-            // 根据距离计算速度
-            const speedX = distX * acceleration;
-            const speedY = distY * acceleration;
-
-            // 更新位置
-            posX += speedX;
-            posY += speedY;
-
-            // 将位置应用到元素
-            block.style.left = `${posX}px`;
-            block.style.top = `${posY}px`;
-
-            // 当方块足够靠近目标位置时，停止动画
-            if (Math.abs(distX) < 1 && Math.abs(distY) < 1) {
-                moving = false;
-                posX = targetX; // 对位置进行微调，确保与目标位置对齐
-                posY = targetY; // 对位置进行微调，确保与目标位置对齐
-                block.style.left = `${posX}px`;
-                block.style.top = `${posY}px`;
-            }
-
-            // 请求下一帧动画
-            requestAnimationFrame(animate);
-        }
+        // 更新方块位置
+        block.style.left = `${posX}px`;
+        block.style.top = `${posY}px`;
     }
 
-    // 监听鼠标点击事件
-    document.addEventListener('mousedown', (e) => {
-        const rect = block.parentElement.getBoundingClientRect(); // 获取game-container的位置和大小
-        // 计算点击位置相对于game-container的位置
-        targetX = e.clientX - rect.left - block.clientWidth / 2;
-        targetY = e.clientY - rect.top - block.clientHeight / 2;
-        moving = true; // 开始移动
-    
-        // 开始动画循环
-        requestAnimationFrame(animate);
-    });
+    // 监听鼠标移动事件，并更新方块位置
+    document.addEventListener('mousemove', followMouse);
 });
