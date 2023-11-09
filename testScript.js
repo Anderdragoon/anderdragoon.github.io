178625f54eabe7b867576d1ceae2ba31e323e209
Anderdragoon <anderdragonltl@outlook.com>
2023-11-08 21:18:43 -0500

    cursor with button

--- testScript.js
+++ testScript.js
@@ -31,3 +31,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // 启动动画循环
     requestAnimationFrame(updatePosition);
 });
+
+document.body.addEventListener('mousedown', function() {
+    document.body.classList.add('click-cursor');
+  });
+
+  // 当鼠标释放时移除class
+  document.body.addEventListener('mouseup', function() {
+    document.body.classList.remove('click-cursor');
+  });
+
+  // 也可以考虑鼠标离开页面的情况
+  document.body.addEventListener('mouseleave', function() {
+    document.body.classList.remove('click-cursor');
+  });
