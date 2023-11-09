210020969c740678112efae604fb089c2b126f43
Anderdragoon <anderdragonltl@outlook.com>
2023-11-08 21:49:43 -0500

    update cursor

--- testScript.js
+++ testScript.js
@@ -1,13 +1,11 @@
-document.body.addEventListener('mousedown', function() {
+window.addEventListener('mousedown', function() {
     document.body.classList.add('click-cursor');
-  });
+});
 
-  // 当鼠标释放时移除class
-  document.body.addEventListener('mouseup', function() {
+window.addEventListener('mouseup', function() {
     document.body.classList.remove('click-cursor');
-  });
+});
 
-  // 也可以考虑鼠标离开页面的情况
-  document.body.addEventListener('mouseleave', function() {
+window.addEventListener('mouseleave', function() {
     document.body.classList.remove('click-cursor');
-  });
+});
