ebbad751ce2c586d1f13ca3197899e3af966f34e
Anderdragoon <anderdragonltl@outlook.com>
2023-11-20 01:26:35 -0500

    ccccc

--- testScript.js
+++ testScript.js
@@ -5,21 +5,8 @@ function scrollToImage(imageId) {
     }
 }
 
-// 当用户滚动一定距离时显示按钮
-window.onscroll = function() {
-    scrollFunction();
-  };
-  
-  function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      document.getElementById("backToTop").style.display = "block";
-    } else {
-      document.getElementById("backToTop").style.display = "none";
-    }
-  }
-  
-  // 当用户点击按钮时，滚动到页面顶部
-  document.getElementById("backToTop").onclick = function() {
+// 当用户点击按钮时，滚动到页面顶部
+document.getElementById("backToTop").onclick = function() {
     document.body.scrollTop = 0; // 对于Safari
     document.documentElement.scrollTop = 0; // 对于Chrome, Firefox, IE和Opera
-  };
+};
