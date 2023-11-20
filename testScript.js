defef6cec6b5b151c09f954064fcd72c7eab9794
Anderdragoon <anderdragonltl@outlook.com>
2023-11-20 01:11:18 -0500

    hhhh

--- testScript.js
+++ testScript.js
@@ -1,23 +1,25 @@
-// 当用户向下滚动100px时，显示按钮
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    console.log("滚动事件触发"); // 用于调试
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-      document.getElementById("topBtn").style.display = "block";
-    } else {
-      document.getElementById("topBtn").style.display = "none";
-    }
-  }
-
-// 当用户点击按钮时，返回页面顶部
-function topFunction() {
-    window.scrollTo({top: 0, behavior: 'smooth'});
-  }
-
 function scrollToImage(imageId) {
     var element = document.getElementById(imageId);
     if(element) {
         element.scrollIntoView({behavior: 'smooth'});
     }
 }
+
+// 当用户滚动一定距离时显示按钮
+window.onscroll = function() {
+    scrollFunction();
+  };
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("backToTop").style.display = "block";
+    } else {
+      document.getElementById("backToTop").style.display = "none";
+    }
+  }
+  
+  // 当用户点击按钮时，滚动到页面顶部
+  document.getElementById("backToTop").onclick = function() {
+    document.body.scrollTop = 0; // 对于Safari
+    document.documentElement.scrollTop = 0; // 对于Chrome, Firefox, IE和Opera
+  };
